8b053ebe2bc1701878fc506c9e5a18c182218d5a
Tayalov <241752@astanait.edu.kz>
2026-02-09 23:12:49 +0500

    Update server.js

--- finalweb/backend/server.js
+++ finalweb/backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(express.static("public"));
 
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -19,15 +20,21 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/finalweb";
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB connected");
 
-mongoose.connect(MONGO_URI)  
-  .then(() => {
-    console.log("MongoDB connected");
+  const PORT = process.env.PORT || 5000;  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch(err => console.error("MongoDB connection error:", err));
 
-    const PORT = process.env.PORT || 5000;  
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch(err => console.error("MongoDB connection error:", err));
+
+
